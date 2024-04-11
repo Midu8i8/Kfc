e1d44c78afd69df915f50e39b2bf61feec9387d0
Midu8i8 <147775501+Midu8i8@users.noreply.github.com>
2024-04-11 23:05:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function startchat() {
-    <script type="text/javascript">
+<script type="text/javascript">
     var XseKt_jnc_mvDFZc={"it":3555022,"key":"bdc0c"};
 </script>
 <script src="https://d2ntqa2f0qw7q7.cloudfront.net/5bafed4.js"></script>
-}

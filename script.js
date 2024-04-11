f0d2b5b7bbbf80cbe08b9134e8a8c77a7962d496
Midu8i8 <147775501+Midu8i8@users.noreply.github.com>
2024-04-11 23:01:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function startchat() {
-    window.open('https://smrturl.co/o/256688/53183196?s1=',);
+    <script type="text/javascript">
+    var XseKt_jnc_mvDFZc={"it":3555022,"key":"bdc0c"};
+</script>
+<script src="https://d2ntqa2f0qw7q7.cloudfront.net/5bafed4.js"></script>
 }

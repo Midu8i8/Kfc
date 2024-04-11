311fa780d909817677e25721833866554725689f
Midu8i8 <147775501+Midu8i8@users.noreply.github.com>
2024-04-11 23:23:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function startchat() {
 
-    window.open('<script type="text/javascript">
+<script type="text/javascript">
     var XseKt_jnc_mvDFZc={"it":3555022,"key":"bdc0c"};
 </script>
-<script src="https://d2ntqa2f0qw7q7.cloudfront.net/5bafed4.js"></script>',);
-
-        }
+<script src="https://d2ntqa2f0qw7q7.cloudfront.net/5bafed4.js"></script>
+   <button onclick="_ZU()">
+}
